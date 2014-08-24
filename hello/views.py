3363b1c47e06e6f4765fbefe8024b360e86c7ae2
Rosette Diaz <rosettekdiaz@Rosettes-MacBook-Pro.local>
2014-08-23 18:48:21 -0700

    initial commit

--- hello/views.py
+++ hello/views.py
@@ -1,19 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse
-
-from .models import Greeting
 
 # Create your views here.
 def index(request):
-    return HttpResponse('Hello from Python!')
-
-
-def db(request):
-
-    greeting = Greeting()
-    greeting.save()
-
-    greetings = Greeting.objects.all()
-
-    return render(request, 'db.html', {'greetings': greetings})
-
+    return renter(request, 'spermies.html')
