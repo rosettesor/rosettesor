9163940b52870747570bbfe22262f49e28e2143d
Rosette Diaz <rkd@norse-corp.com>
2016-02-15 17:13:17 -0800

    add rainbow challenge

--- hello/views.py
+++ hello/views.py
@@ -3,3 +3,6 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request, 'spermies.html')
+
+def nfl_rainbow(request):
+    return render(request, 'nfl_rainbow.html')
